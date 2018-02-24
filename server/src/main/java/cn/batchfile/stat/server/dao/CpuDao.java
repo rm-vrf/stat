f8919554644c067e23fcebedce44acd6e625b02d
htlu <htlu@192.168.1.100>
2018-02-24 10:50:02 +0800

    change resource unit(disk & mem)

--- server/src/main/java/cn/batchfile/stat/server/dao/CpuDao.java
+++
@@ -1,8 +0,0 @@
-//package cn.batchfile.stat.server.dao;
-//
-//import cn.batchfile.stat.server.domain.CpuData;
-//
-//public interface CpuDao {
-//
-//	void insertData(CpuData cpuData);
-//}
--- server/src/main/java/cn/batchfile/stat/server/dao/MemoryDao.java
+++
@@ -1,8 +0,0 @@
-//package cn.batchfile.stat.server.dao;
-//
-//import cn.batchfile.stat.server.domain.MemoryData;
-//
-//public interface MemoryDao {
-//	
-//	void insertData(MemoryData memoryData);
-//}
