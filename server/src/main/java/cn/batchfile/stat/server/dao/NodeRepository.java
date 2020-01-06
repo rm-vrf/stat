74670e4a16a9ae41756af729023cdb8485965df7
htlu <htlu@iflytek.com>
2020-01-06 17:49:32 +0800

    forgot file

---
+++ server/src/main/java/cn/batchfile/stat/server/dao/NodeRepository.java
@@ -0,0 +1,18 @@
+package cn.batchfile.stat.server.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import cn.batchfile.stat.server.dto.NodeTable;
+
+public interface NodeRepository extends CrudRepository<NodeTable, String> {
+
+	@Query("SELECT n FROM Node n ORDER BY dockerHost")
+	Iterable<NodeTable> findMany();
+	
+	@Query("SELECT n FROM Node n WHERE dockerHost=:dockerHost")
+	Optional<NodeTable> findOne(@Param("dockerHost") String dockerHost);
+}
